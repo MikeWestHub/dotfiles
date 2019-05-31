0ab9701ccc091f24f02fc56c835bb39280ff2914
Mike West <westmd23@gmail.com>
2019-05-31 17:48:13 -0400

    Add git ignore and install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dotfiles="$HOME/dotfiles"
+files="gitconfig gitignore tmux.conf vimrc zshenv zshrc"
+
+for file in $files; do
+  rm $HOME/.$file
+  ln -s $dotfiles/$file $HOME/.$file
+done
